b2231aed12712c0a1fa2330b303da2fc13256df4
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-08-16 12:14:08 +0300

    Change listener port from 80 to 8080

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 #include <drogon/drogon.h>
 
 int main() {
-  drogon::app().addListener("0.0.0.0", 80);
+  drogon::app().addListener("0.0.0.0", 8080);
   drogon::app().loadConfigFile("../config.json");
   drogon::app().run();
   return 0;
